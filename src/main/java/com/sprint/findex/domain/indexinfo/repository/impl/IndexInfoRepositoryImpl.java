80f269dc5a55e3f41fedd56b544e05cfa61b8a78
이수찬 <lsc0869@naver.com>
2026-09-09 01:21:09 +0900

    feat: 지수 정보 등록용 Repository 및 중복 체크 메서드 추가

--- src/main/java/com/sprint/findex/domain/indexinfo/repository/impl/IndexInfoRepositoryImpl.java
+++
@@ -1,5 +0,0 @@
-package com.sprint.findex.domain.indexinfo.repository.impl;
-
-import com.sprint.findex.domain.indexinfo.repository.IndexInfoRepository;
-
-public class IndexInfoRepositoryImpl implements IndexInfoRepository {}
