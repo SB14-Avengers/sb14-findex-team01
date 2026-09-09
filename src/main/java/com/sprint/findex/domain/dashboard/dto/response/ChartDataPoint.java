6f5a568f09cefda0e04fc83ec0acc7678d04c219
yyy2724 <yyy2724@naver.com>
2026-09-09 17:36:30 +0900

    feat: 지수 정보 요약 조회 구현 (#19)
    # Conflicts:
    #	src/main/java/com/sprint/findex/domain/indexdata/repository/IndexDataRepository.java

--- src/main/java/com/sprint/findex/domain/dashboard/dto/response/ChartDataPoint.java
+++ src/main/java/com/sprint/findex/domain/dashboard/dto/response/ChartDataPoint.java
@@ -1,6 +1,13 @@
 package com.sprint.findex.domain.dashboard.dto.response;
 
+import com.sprint.findex.domain.indexdata.entity.IndexData;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
-public record ChartDataPoint(LocalDate date, BigDecimal value) {}
+public record ChartDataPoint(LocalDate date, BigDecimal value) {
+
+    // 종가 반환
+    public static ChartDataPoint from(IndexData data) {
+        return new ChartDataPoint(data.getBaseDate(), data.getClosingPrice());
+    }
+}
