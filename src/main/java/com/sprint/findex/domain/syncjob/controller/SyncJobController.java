51cbdc029b2213de8b590ec44b49db9f25debdea
김예준 <72857855+kim-yejunn@users.noreply.github.com>
2026-09-09 14:47:45 +0900

    feat: controller 뼈대 생성 (#11)
    - SyncJobApi 생성하여 swagger 문서 작성
    - Controller작성 후 서비스 작성하여 연결

--- src/main/java/com/sprint/findex/domain/syncjob/controller/SyncJobController.java
+++ src/main/java/com/sprint/findex/domain/syncjob/controller/SyncJobController.java
@@ -1,3 +1,32 @@
 package com.sprint.findex.domain.syncjob.controller;
 
-public class SyncJobController {}
+import com.sprint.findex.domain.syncjob.dto.response.SyncJobDto;
+import com.sprint.findex.domain.syncjob.service.SyncJobService;
+import com.sprint.findex.global.common.CursorPageResponse;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class SyncJobController implements SyncJobApi {
+    private final SyncJobService syncJobService;
+
+    @Override
+    public ResponseEntity<List<SyncJobDto>> indexInfoSync() {
+        List<SyncJobDto> syncJobs = syncJobService.indexInfoSync();
+        return ResponseEntity.accepted().body(syncJobs);
+    }
+
+    @Override
+    public ResponseEntity<List<SyncJobDto>> indexDataSync() {
+        List<SyncJobDto> syncJobs = syncJobService.indexDataSync();
+        return ResponseEntity.accepted().body(syncJobs);
+    }
+
+    @Override
+    public ResponseEntity<CursorPageResponse<SyncJobDto>> findSyncJobs() {
+        return null;
+    }
+}
--- src/main/java/com/sprint/findex/domain/syncjob/service/impl/SyncJobServiceImpl.java
+++ src/main/java/com/sprint/findex/domain/syncjob/service/impl/SyncJobServiceImpl.java
@@ -1,3 +1,21 @@
 package com.sprint.findex.domain.syncjob.service.impl;
 
-public class SyncJobServiceImpl {}
+import com.sprint.findex.domain.syncjob.dto.response.SyncJobDto;
+import com.sprint.findex.domain.syncjob.service.SyncJobService;
+import java.util.List;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SyncJobServiceImpl implements SyncJobService {
+    @Override
+    public List<SyncJobDto> indexInfoSync() {
+        // 지수 정보 가져와서 연동하는 로직 구현
+        return null;
+    }
+
+    @Override
+    public List<SyncJobDto> indexDataSync() {
+        // 지수 데이터 가져와서 연동하는 로직 구현
+        return null;
+    }
+}
