57cb64f6d2cb78a5078318180a2d7e7c826cfc91
yyy2724 <yyy2724@naver.com>
2026-09-10 15:12:56 +0900

    feat: 이동평균선 계산 로직 구현 (#23)

---
+++ src/test/java/com/sprint/findex/domain/dashboard/service/impl/DashboardServiceImplTest.java
@@ -0,0 +1,11 @@
+package com.sprint.findex.domain.dashboard.service.impl;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class DashboardServiceImplTest {
+
+    @Test
+    void getChart() {}
+}
