0771527ca7cc194587e558f83a7b5ae710447596
yyy2724 <yyy2724@naver.com>
2026-09-10 14:41:57 +0900

    feat: 이동평균선 계산 로직 구현 (#23)

--- src/main/java/com/sprint/findex/domain/dashboard/dto/response/ChartDataPoint.java
+++ src/main/java/com/sprint/findex/domain/dashboard/dto/response/ChartDataPoint.java
@@ -1,18 +1,6 @@
 package com.sprint.findex.domain.dashboard.dto.response;
 
-import com.sprint.findex.domain.indexdata.entity.IndexData;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
-public record ChartDataPoint(LocalDate date, BigDecimal value) {
-
-    // 종가 반환
-    public static ChartDataPoint from(IndexData data) {
-        return new ChartDataPoint(data.getBaseDate(), data.getClosingPrice());
-    }
-
-    // 평균값 반환
-    public static ChartDataPoint of(LocalDate date, BigDecimal value) {
-        return new ChartDataPoint(date, value);
-    }
-}
+public record ChartDataPoint(LocalDate date, BigDecimal value) {}
