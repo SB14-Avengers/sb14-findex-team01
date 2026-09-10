15e81b14ab24277bccc3867366562bb6ebf4f75e
이수찬 <lsc0869@naver.com>
2026-09-10 16:49:01 +0900

    fix: 지수 정보 수정 API 입력값 검증 추가

--- src/main/java/com/sprint/findex/domain/indexinfo/dto/request/IndexInfoUpdateRequest.java
+++ src/main/java/com/sprint/findex/domain/indexinfo/dto/request/IndexInfoUpdateRequest.java
@@ -1,11 +1,12 @@
 package com.sprint.findex.domain.indexinfo.dto.request;
 
+import jakarta.validation.constraints.Positive;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
 public record IndexInfoUpdateRequest(
-        Integer employedItemsCount, // 채용 종목 수
+        @Positive Integer employedItemsCount, // 채용 종목 수
         LocalDate basePointInTime, // 기준 시점
-        BigDecimal baseIndex, // 기준 지수
+        @Positive BigDecimal baseIndex, // 기준 지수
         Boolean favorite // 즐겨찾기
         ) {}
