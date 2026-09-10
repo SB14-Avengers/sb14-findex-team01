0b34e160ae27e98bfd59d55bb0899cb9a2e0f522
SungJun <xian980@naver.com>
2026-09-10 11:23:21 +0900

    fix: 컨트롤러 구현체 파라미터 어노테이션 중복제거, 서비스 구현체 @Transactional추가

--- src/main/java/com/sprint/findex/domain/indexdata/controller/IndexDataController.java
+++ src/main/java/com/sprint/findex/domain/indexdata/controller/IndexDataController.java
@@ -3,7 +3,6 @@ package com.sprint.findex.domain.indexdata.controller;
 import com.sprint.findex.domain.indexdata.dto.request.IndexDataCreateRequest;
 import com.sprint.findex.domain.indexdata.dto.response.IndexDataDto;
 import com.sprint.findex.domain.indexdata.service.IndexDataService;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,9 +14,8 @@ public class IndexDataController implements IndexDataApi {
 
     private final IndexDataService indexDataService;
 
-    @PostMapping
-    public ResponseEntity<IndexDataDto> create(
-            @Valid @RequestBody IndexDataCreateRequest createRequest) {
+    @Override
+    public ResponseEntity<IndexDataDto> create(IndexDataCreateRequest createRequest) {
         IndexDataDto indexDataDto = indexDataService.create(createRequest);
         return ResponseEntity.status(HttpStatus.CREATED).body(indexDataDto);
     }
