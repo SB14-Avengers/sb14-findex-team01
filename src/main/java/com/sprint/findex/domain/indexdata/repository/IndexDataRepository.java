0a45460afe26d512a202b3025b84613c2057caa6
yyy2724 <yyy2724@naver.com>
2026-09-10 13:48:47 +0900

    feat: 이동평균선 계산 로직 구현 (#23)

--- src/main/java/com/sprint/findex/domain/indexdata/repository/IndexDataRepository.java
+++ src/main/java/com/sprint/findex/domain/indexdata/repository/IndexDataRepository.java
@@ -4,10 +4,21 @@ import com.sprint.findex.domain.indexdata.entity.IndexData;
 import java.time.LocalDate;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface IndexDataRepository extends JpaRepository<IndexData, Long> {
     List<IndexData> findByIndexInfoIdOrderByBaseDateAsc(Long indexInfoId);
 
     boolean existsByIndexInfoIdAndBaseDate(Long indexInfoId, LocalDate baseDate);
 
+    List<IndexData> findByIndexInfoIdAndBaseDateGreaterThanEqualOrderByBaseDate(
+            Long indexInfoId, LocalDate baseDate);
+
+    @Query(
+            "select d from IndexData d \n"
+                    + "where d.indexInfo.id = :indexInfoId \n"
+                    + "and d.baseDate >= :baseDate \n"
+                    + "order by d.baseDate asc")
+    List<IndexData> findChartData(@Param("indexInfoId") Long id, @Param("baseDate") LocalDate data);
 }
