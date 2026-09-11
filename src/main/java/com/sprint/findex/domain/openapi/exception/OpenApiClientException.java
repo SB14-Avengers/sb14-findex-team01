25e5420bfe4ad6e96d93bf5cc3726b601a43c6b6
WinLike <kimseoungho0219@gmail.com>
2026-09-11 14:30:48 +0900

    chore: rebase 전 변경사항 저장

--- src/main/java/com/sprint/findex/domain/openapi/exception/OpenApiClientException.java
+++ src/main/java/com/sprint/findex/domain/openapi/exception/OpenApiClientException.java
@@ -5,7 +5,7 @@ import lombok.Getter;
 /**
  * syncjob에서 잡아 실패 이력을 기록하는 예외.
  *
- * 서비스 키가 포함된 URL이 노출될 수 있으므로 원인 예외를 연결하거나 메시지에 요청 URL을 넣지 않는다.
+ * <p>서비스 키가 포함된 URL이 노출될 수 있으므로 원인 예외를 연결하거나 메시지에 요청 URL을 넣지 않는다.
  */
 @Getter
 public class OpenApiClientException extends RuntimeException {
